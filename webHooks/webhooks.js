92500e7dc9b716c54faf2ad1a1f9f74775ee75b2
roman-jordan <rjordan1352@gmail.com>
2019-10-30 19:43:47 -0700

    Does the Secret Match? With Crypto

--- webHooks/webhooks.js
+++ webHooks/webhooks.js
@@ -1,10 +1,18 @@
 const router = require("express").Router();
 const { spawn } = require("child_process");
+let crypto = require('crypto');
 const gitSecret = process.env.GIT_SECRET;
 
 router.post("/github", (req, res) => {
-    console.log('BODY', req.body.secret)
-    
+  const sig =
+    "sha1=" +
+    crypto
+      .createHmac("sha1", gitSecret)
+      .update(chunk.toString())
+      .digest("hex");
+
+  //The Secret Matches
+  if (req.headers["x-hub-signature"] == sig) {
     const gitPull = spawn("git", ["pull"]);
 
     gitPull.stdout.on("data", data => {
@@ -18,7 +26,9 @@ router.post("/github", (req, res) => {
     gitPull.stdout.on("close", data => {
       res.status(200).json({ thankyou: "github" }); //End the stream on close
     });
-  
-}); 
+  } else { //Bad Signature
+    res.status(401).json({message:'Not today Spider-Man',error:'Bad Signature'});
+  }
+});
 
 module.exports = router;
