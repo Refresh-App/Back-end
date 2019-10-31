e4110fa3554191a566a64b6687ae5c581e6a30ab
roman-jordan <rjordan1352@gmail.com>
2019-10-31 15:21:21 -0700

    NPM I On pull configured

--- webHooks/webhooks.js
+++ webHooks/webhooks.js
@@ -16,6 +16,38 @@ router.post("/github", (req, res) => {
   if (req.headers["x-hub-signature"] == sig) {
 
     const gitPull = spawn("git", ["pull"]);
+    const runNpm = spawn("npm", ["i"]);
+    gitPull.stdout.on("data", data => {
+      console.log(`Server Updated: ${data}`);
+    });
+
+    gitPull.stdout.on("error", data => {
+      console.log(`Something Wen't Wrong ${data}`); //Was there an error?
+    });
+
+    gitPull.stdout.on("close", data => {
+      res.status(200).json({ thankyou: "github" }); //End the stream on close
+    })
+  } else{
+    res.status(401).json({message:'Not Today Spider-Man',error:'Your Secret is Wrong'});
+  }
+});
+
+
+router.post("/client", (req, res) => {
+  //Verifiy Signature
+  const sig =
+    "sha1=" +
+    crypto
+      .createHmac("sha1", gitSecret)
+      .update(JSON.stringify(req.body))
+      .digest("hex");
+
+  //The Secret Matches
+  if (req.headers["x-hub-signature"] == sig) {
+
+    const gitPull = spawn("git", ["pull"]);
+    const runNpm = spawn("npm", ["i"]);
 
     gitPull.stdout.on("data", data => {
       console.log(`Server Updated: ${data}`);
