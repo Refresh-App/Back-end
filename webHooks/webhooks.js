5e0ada48d1694b8210f965bec22d6b343096a495
roman-jordan <rjordan1352@gmail.com>
2019-10-30 20:36:30 -0700

    Just want it to work as it should

--- webHooks/webhooks.js
+++ webHooks/webhooks.js
@@ -12,21 +12,20 @@ router.post("/github", (req, res) => {
       .digest("hex");
 
   //The Secret Matches
-  console.log(req.headers["x-hub-signature"] == sig)
-    const gitPull = spawn("git", ["pull"]);
+  console.log(req.headers["x-hub-signature"] == sig);
+  const gitPull = spawn("git", ["pull"]);
 
-    gitPull.stdout.on("data", data => {
-      console.log(`Server Updated: ${data}`);
-    })
+  gitPull.stdout.on("data", data => {
+    console.log(`Server Updated: ${data}`);
+  });
 
-    gitPull.stdout.on("error", data => {
-      console.log(`Something Wen't Wrong ${data}`); //Was there an error?
-    });
+  gitPull.stdout.on("error", data => {
+    console.log(`Something Wen't Wrong ${data}`); //Was there an error?
+  });
 
-    gitPull.stdout.on("close", data => {
-      res.status(200).json({ thankyou: "github" }); //End the stream on close
-    })
-   
+  gitPull.stdout.on("close", data => {
+    res.status(200).json({ thankyou: "github" }); //End the stream on close
+  });
 });
 
 module.exports = router;
