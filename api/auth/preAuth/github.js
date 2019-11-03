5ebe093201448eb7c194707d31dc6906c794ffda
roman-jordan <rjordan1352@gmail.com>
2019-11-03 15:58:44 -0800

    da funk is goinging on here

--- api/auth/preAuth/github.js
+++ api/auth/preAuth/github.js
@@ -36,7 +36,7 @@ gitHubRouter.get("/gitAuth", passport.authenticate("github"));
 
 gitHubRouter.get(
   "/gitAuthReturn",
-  passport.authenticate("github", { failureRedirect: "/login" }),
+  passport.authenticate("github", { failureRedirect: "/login",session:false }),
   (req, res) => {
     console.log("req", req.user);
     res.json({ message: "logged in", ...req.user });
