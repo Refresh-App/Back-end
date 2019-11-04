c87692c0e2fed8c181b7d84770b71b1021792c4a
roman-jordan <rjordan1352@gmail.com>
2019-11-04 06:56:43 -0800

    git hub login g2g

--- api/auth/preAuth/github.js
+++ api/auth/preAuth/github.js
@@ -23,11 +23,7 @@ passport.use(
     },
     function(accessToken, refreshToken, profile, done) {
       console.log(accessToken)
-      User.addUser({ username: profile.username, password: "3334d44" })
-        .then(res => {
           done(null,profile,accessToken);
-        })
-        .catch(err => done(err));
     }
   )
 );
