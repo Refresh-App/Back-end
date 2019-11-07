c03f064d641569aa6f9ede32bce7b6ff70729183
roman-jordan <rjordan1352@gmail.com>
2019-11-07 00:44:21 -0800

    yo

--- api/auth/preAuth/google.js
+++ api/auth/preAuth/google.js
@@ -35,6 +35,7 @@ googleRouter.get(
   passport.authenticate("google", {
     scope: [
       "https://www.googleapis.com/auth/plus.login",
+      'https://www.googleapis.com/auth/userinfo.email'
     ],
     session:false
   })
