db4a7e913509a86aedd8cb212a6ec4cef4a65f70
roman-jordan <rjordan1352@gmail.com>
2019-11-07 00:03:35 -0800

    maybe now

--- api/auth/preAuth/google.js
+++ api/auth/preAuth/google.js
@@ -1,7 +1,7 @@
 const googleRouter = require("express").Router();
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth2").Strategy;
-const jwt = require(_jwt)
+const jwt = require(_jwt);
 //Config GitHub Auth
 const googleId = process.env.GOOGLE_CLIENT_ID;
 const googleSecret = process.env.GOOGLE_CLIENT_SECRET;
@@ -20,8 +20,7 @@ passport.use(
       clientID: googleId,
       consumerSecret: googleSecret,
       callbackURL: googleRedirect,
-      session: false,
-      
+      session: false
     },
     function(accessToken, refreshToken, profile, done) {
       console.log(accessToken);
@@ -31,33 +30,49 @@ passport.use(
 );
 
 //GitLogin URL
-googleRouter.get("/", passport.authenticate("google"));
+googleRouter.get(
+  "/",
+  passport.authenticate("google", {
+    scope: [
+      "https://www.googleapis.com/auth/plus.login",
+      ,
+      "https://www.googleapis.com/auth/plus.profile.emails.read"
+    ],
+    session:false
+  })
+);
 
 //Github Call Back
 googleRouter.get(
   "/return",
   passport.authenticate("google", {
     failureRedirect: "/login",
-    session: false,
+    session: false
   }),
   (req, res) => {
     console.log("req", req.user);
     //...So, not sure how to deal with escaping very well. R-J
-    delete req.user._raw
-    delete req.user._json.bio
+    delete req.user._raw;
+    delete req.user._json.bio;
     const setToken = `
     <script>
       (function(){
         window.opener.postMessage('${JSON.stringify(req.user)}', "*");
         window.close()
       })()
-    </script>`
-    res.set('Content-Type', 'text/html');
-    res.send(Buffer.from(setToken))
-  });
+    </script>`;
+    res.set("Content-Type", "text/html");
+    res.send(Buffer.from(setToken));
+  }
+);
 
-  googleRouter.get("/terms", (req,res) =>{
-    res.status(200).json({message:'Pretty much, we use your email to create your account, if you want to disconnect from your registration median, select forgot password and you will be switched to our local auth stratagey. If you want to nuke your account, go to settings and click the nuke button. We will do our best to protect your data, however, where there is a will there is a way. That being said, we take zeor liablity for any data breaches. Your data may be used for internal and external purposes, but, mainly to improve our product for you.'});
-  });
+googleRouter.get("/terms", (req, res) => {
+  res
+    .status(200)
+    .json({
+      message:
+        "Pretty much, we use your email to create your account, if you want to disconnect from your registration median, select forgot password and you will be switched to our local auth stratagey. If you want to nuke your account, go to settings and click the nuke button. We will do our best to protect your data, however, where there is a will there is a way. That being said, we take zeor liablity for any data breaches. Your data may be used for internal and external purposes, but, mainly to improve our product for you."
+    });
+});
 
 module.exports = googleRouter;
