1ce300db5317a16287746eda88e569b9a3f11ac2
roman-jordan <rjordan1352@gmail.com>
2019-11-06 23:32:52 -0800

    Maybe Now

--- api/auth/preAuth/google.js
+++ api/auth/preAuth/google.js
@@ -39,7 +39,6 @@ googleRouter.get(
   passport.authenticate("google", {
     failureRedirect: "/login",
     session: false,
-    scope: ['email']
   }),
   (req, res) => {
     console.log("req", req.user);
