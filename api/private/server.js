68975eba8c86dac4321d00072c444c5346e1f6b9
Jeremy Brady <jbdigitalgeek@gmail.com>
2019-11-13 15:27:05 -0800

    wired up users w/functions --untested

--- api/private/server.js
+++ api/private/server.js
@@ -1,8 +1,14 @@
 const privateRouter = require("express").Router();
 const mailRouter = require("./mailer/sendMail");
 const adminRouter = require("./admin/admin");
+const userRouter = require("./users/users");
+const rolesRouter = require("./roles/roles")
+
+
 const jwt = require(_jwt);
 
 privateRouter.use("/mailerYo", mailRouter);
 privateRouter.use("/admin", jwt.chkRole(), adminRouter);
+privateRouter.use("/users", userRouter);
+privateRouter.use("/roles", rolesRouter);
 module.exports = privateRouter;
--- api/public/server.js
+++ api/public/server.js
@@ -1,6 +1,4 @@
 const publicRouter = require("express").Router();
-const rolesRouter = require("./roles/roles")
 
-publicRouter.use("/roles", rolesRouter);
 
 module.exports = publicRouter;
