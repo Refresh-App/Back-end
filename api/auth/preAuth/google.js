fa9ec098128eb5e95dd07fcfb2d986a7abfa8abd
roman-jordan <rjordan1352@gmail.com>
2019-11-12 16:29:29 -0800

    updated console.log

--- api/auth/preAuth/google.js
+++ api/auth/preAuth/google.js
@@ -28,7 +28,7 @@ passport.use(
       profile = profileScrubber(profile)
       User.findOrCreateByEmail(profile)
       .then(res =>{
-        done(null,profile, accessToken)
+        done(null,res, accessToken)
       })
     }
   )
