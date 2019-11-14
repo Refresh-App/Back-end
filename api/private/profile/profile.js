b5e11439c235871e1d3cb4f932d6287c321b86ee
roman-jordan <rjordan1352@gmail.com>
2019-11-13 22:14:27 -0800

    Updated Router

--- api/private/profile/profile.js
+++ api/private/profile/profile.js
@@ -4,7 +4,7 @@ const dbModel = require("./profileModle");
 router.get("/", (req, res) => {
   console.log(req.user)
   return dbModel
-    .findAllById(req.user.id)
+    .findByUserId(req.user.id)
     .then(p => {
       res.status(200).json({ message: `SUCCESS`, ...p });
     })
