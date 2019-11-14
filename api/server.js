9907f996b6b0b9f411cd50572c21aa882de6518b
Jeremy Brady <jbdigitalgeek@gmail.com>
2019-11-13 17:50:30 -0800

    rewiring roles table endpoints

--- api/server.js
+++ api/server.js
@@ -1,19 +1,20 @@
 const primaryRouter = require("express").Router();
 
 //Authentication
-const jwt = require('./auth/preAuth/jwt')
+const jwt = require("./auth/preAuth/jwt");
 
 //Bring in the Routes
 const publicRouter = require("./public/server");
 const privateRouter = require("./private/server");
-//Login, Register, GoogleAuth, FaceBookAuth, GitHubAuth 
-const authRouter = require("./auth/auth") 
+const roleRouter = require("./public/roles/roles");
+
+//Login, Register, GoogleAuth, FaceBookAuth, GitHubAuth
+const authRouter = require("./auth/auth");
 
 //Implement Routes
-primaryRouter.use('/',authRouter)
-primaryRouter.use('/public',publicRouter)
-primaryRouter.use('/',jwt.chkToken(),privateRouter)
-
-
+primaryRouter.use("/", authRouter);
+primaryRouter.use("/public", publicRouter);
+primaryRouter.use("/public/roles", rolesRouter);
+primaryRouter.use("/", jwt.chkToken(), privateRouter);
 
 module.exports = primaryRouter;
