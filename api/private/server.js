5adb685bd92258ef402162ccce25cbae5984b1a5
Jeremy Brady <jbdigitalgeek@gmail.com>
2019-11-13 21:13:47 -0800

    wired in questions router

--- api/private/server.js
+++ api/private/server.js
@@ -2,7 +2,8 @@ const privateRouter = require("express").Router();
 const mailRouter = require("./mailer/sendMail");
 const adminRouter = require("./admin/admin");
 const userRouter = require("./users/users");
-const rolesRouter = require("../public/roles/roles")
+const rolesRouter = require("../public/roles/roles");
+const questionsRouter = require("./questions/questions")
 
 
 const jwt = require(_jwt);
@@ -11,5 +12,6 @@ privateRouter.use("/mailerYo", mailRouter);
 privateRouter.use("/admin", jwt.chkRole(), adminRouter);
 privateRouter.use("/users", userRouter);
 privateRouter.use("/roles", rolesRouter);
+privateRouter.use("/questions", questionsRouter);
 
 module.exports = privateRouter;
