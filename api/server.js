2bdd28b628e81ba99e3a2f73689dad8f4e5eefaa
roman-jordan <rjordan1352@gmail.com>
2019-11-13 21:05:28 -0800

    updated Routing

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ const jwt = require("./auth/preAuth/jwt");
 //Bring in the Routes
 const publicRouter = require("./public/server");
 const privateRouter = require("./private/server");
-const roleRouter = require("./public/roles/roles");
+
 
 //Login, Register, GoogleAuth, FaceBookAuth, GitHubAuth
 const authRouter = require("./auth/auth");
