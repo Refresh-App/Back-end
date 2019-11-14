680f24b487e3a266d0f15ba945314fb6b849a571
Jeremy Brady <jbdigitalgeek@gmail.com>
2019-11-13 21:37:57 -0800

    wired in missin router

--- api/private/server.js
+++ api/private/server.js
@@ -4,6 +4,7 @@ const adminRouter = require("./admin/admin");
 const userRouter = require("./users/users");
 const questionsRouter = require("./questions/questions");
 const profileRouter = require("./profile/profile");
+const missionRouter = require("./missions/missions");
 
 const jwt = require(_jwt);
 
@@ -12,5 +13,6 @@ privateRouter.use("/admin", jwt.chkRole(), adminRouter);
 privateRouter.use("/users", userRouter);
 privateRouter.use("/questions", questionsRouter);
 privateRouter.use("/profile", profileRouter);
+privateRouter.use("/missions", missionRouter);
 
 module.exports = privateRouter;
