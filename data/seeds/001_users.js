2ac0f590df49897210c7875c06a0180db13afc45
Jeremy Brady <jbdigitalgeek@gmail.com>
2019-11-13 23:20:25 -0800

    seed data testing

--- data/seeds/001_users.js
+++ data/seeds/001_users.js
@@ -1,9 +1,9 @@
 exports.seed = function(knex) {
     // Deletes ALL existing entries
-    return knex('user_roles').truncate()
+    return knex('users').truncate()
       .then(function () {
         // Inserts seed entries
-        return knex('user_roles').insert([
+        return knex('users').insert([
           {email: "email@MediaList.com", password: "password"},
           {email: "email@testmail.com", password: "password"},
    
