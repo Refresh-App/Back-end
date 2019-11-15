140d3b8765f2087190d42a04495297de63437268
roman-jordan <rjordan1352@gmail.com>
2019-11-15 11:52:56 -0800

    Updated Jwt Token

--- api/auth/authModel.js
+++ api/auth/authModel.js
@@ -37,7 +37,7 @@ async function findOrCreateByEmail(profile) {
   //If the user exist
   if (user) {
     const  getUserRoles = await rolesModel.findAllRolesById(user.id)
-    return { ...profile,userId:user.id,userRoles:[...getUserRoles], message: "Welcome Back" };
+    return { ...profile,user_id:user.id,userRoles:[...getUserRoles], message: "Welcome Back" };
   } else {//CREATE NEW USER
     
     //Encrypt Password, consider doing off AccessToken
@@ -51,7 +51,7 @@ async function findOrCreateByEmail(profile) {
     delete profile.email
     
     const newProfile = await Profile.createProfile({
-      userId:newUser.id,
+      user_id:newUser.id,
       ...profile,
     })
     
