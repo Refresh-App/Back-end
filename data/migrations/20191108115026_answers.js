10f1da441c0dec3d37ea4b4001e34384b7d1a536
roman-jordan <rjordan1352@gmail.com>
2019-11-19 05:07:08 -0800

    updated knex

--- data/migrations/20191108115026_answers.js
+++ data/migrations/20191108115026_answers.js
@@ -1,18 +1,30 @@
-return knex.schema.createTable("answers", col => {
-  col.increments();
-  col.string("answer", 3000);
-  col.dateTime("answer_date");
-  col
-    .integer("question")
-    .references("id")
-    .inTable("questions")
-    .onDelete("CASCADE");
-  col
-    .integer("user_id")
-    .unsigned()
-    .references("id")
-    .inTable("users");
-});
+
+  
+  
+  
+  
+  
+  exports.up = function(knex) {
+    return knex.schema
+      .createTable("answers", col => {
+        col.increments();
+        col.string("answer", 3000);
+        col.dateTime("answer_date");
+        col
+          .integer("question")
+          .references("id")
+          .inTable("questions")
+          .onDelete("CASCADE");
+        col
+          .integer("user_id")
+          .unsigned()
+          .references("id")
+          .inTable("users");
+      })
+  };
+  
+
+
 
 exports.down = function(knex) {
   return knex.schema
