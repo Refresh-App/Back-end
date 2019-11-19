20ff6493dd94c7a767b75292f62c72e5d2a59242
roman-jordan <rjordan1352@gmail.com>
2019-11-19 05:08:57 -0800

    updated knex

--- data/migrations/20191112193657_mission-model.js
+++ data/migrations/20191112193657_mission-model.js
@@ -1,10 +1,5 @@
 exports.up = function(knex) {
   return knex.schema
-    .createTable("days_of_the_week", col => {
-      col.increments();
-      col.string("day_long").unique();
-      col.string("day_short").unique();
-    })
     .createTable("missions", col => {
       col.increments();
       col.string("vertical").notNullable();
