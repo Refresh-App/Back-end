6468703301743b5950ff1641471003ed3513833a
roman-jordan <rjordan1352@gmail.com>
2019-11-20 11:57:34 -0800

    updated fetch model

--- api/private/answers/answersModel.js
+++ api/private/answers/answersModel.js
@@ -31,6 +31,8 @@ function findByDateRang(startDate, endDate) {
   return db(table)
     .whereBetween("answer_date", [startDate, endDate])
     .orderBy("user_id")
+    .then(res=>console.log(res))
+    .catch(err => console.log(err))
 }
 
 function findByUserId(id) {
