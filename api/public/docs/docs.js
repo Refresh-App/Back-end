2553268bce56e36f0bd3b8cf5b07c67789e02bfc
roman-jordan <rjordan1352@gmail.com>
2019-11-19 16:00:26 -0800

    Html Docs Ready to go

--- api/public/docs/docs.js
+++ api/public/docs/docs.js
@@ -1,8 +1,11 @@
 const router = require('express').Router()
+var path = require('path');
 
 router
   .get('/',(req,res)=>{
-    res.status(200).sendFile('./docs.html')
+    res.type('.html')
+      console.log('here')
+    res.sendFile(path.join(__dirname + '/docs.html'))
 })
 
 module.exports=router
