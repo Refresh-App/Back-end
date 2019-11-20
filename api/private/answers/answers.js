1714ffb3c157ada810f6b7cf6c54bff90e4bdd13
roman-jordan <rjordan1352@gmail.com>
2019-11-20 11:52:26 -0800

    updated fetch model

--- api/private/answers/answers.js
+++ api/private/answers/answers.js
@@ -30,7 +30,7 @@ router.post("/datefilter", (req, res) => {
   const id = req.user.userId;
   const { startDate, endDate } = req.body;
 
-
+  console.log('here',startDate,endDate)
     //THIS SHOULD BE A POST {startDate:'2019-01-01'; endDate:'2019-02-02'};
     return dbModel
       .findBYDateRange(startDate, endDate)
