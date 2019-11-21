9608f9cd4719462d114fc63e0e94ad880508f77b
roman-jordan <rjordan1352@gmail.com>
2019-11-21 09:00:58 -0800

    Updated Missions

--- data/seeds/008-userMissions.js
+++ data/seeds/008-userMissions.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('user_missions').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('user_missions').insert([
         {user_id: 1, mission_id: 1},
         {user_id: 2, mission_id: 2},
         {user_id: 2, mission_id: 1},
