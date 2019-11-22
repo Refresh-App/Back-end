03486a7d4305dcd02a7ba913ae2371c200aab205
roman-jordan <rjordan1352@gmail.com>
2019-11-21 23:33:36 -0800

    yo

--- data/seeds/003_answers.js
+++ data/seeds/003_answers.js
@@ -11,6 +11,7 @@ exports.seed = function(knex) {
         {answer: 2, question_id: 12, user_id: 1, answer_date: '2019-11-19T20:15:36.399Z'},
         {answer: 1, question_id: 11, user_id: 1, answer_date: '2019-11-19T20:15:36.399Z'},
         {answer: 1, question_id: 11, user_id: 2, answer_date: '2019-11-19T20:15:36.399Z'},
+        {answer: 2, question_id: 11, user_id: 2, answer_date: '2019-11-19T20:15:36.399Z'},
 
       ]);
     });
