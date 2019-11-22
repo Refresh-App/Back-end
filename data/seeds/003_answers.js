a320b0d8f28080012312dd1c2318149519850cb9
roman-jordan <rjordan1352@gmail.com>
2019-11-22 00:58:59 -0800

    update

--- data/seeds/003_answers.js
+++ data/seeds/003_answers.js
@@ -3,16 +3,29 @@ exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('answers').del()
     .then(function () {
-      // Inserts seed entries
-      return knex('answers').insert([
-        {answer: 2, question_id: 2, user_id: 2, answer_date: '2019-11-19T20:15:36.399Z'},
-        {answer: 2, question_id: 2, user_id: 2, answer_date: '2019-11-19T20:15:36.399Z'},
-        {answer: 2, question_id: 2, user_id: 2, answer_date: '2019-11-19T20:15:36.399Z'},
-        {answer: 2, question_id: 12, user_id: 1, answer_date: '2019-11-19T20:15:36.399Z'},
-        {answer: 1, question_id: 11, user_id: 1, answer_date: '2019-11-19T20:15:36.399Z'},
-        {answer: 1, question_id: 11, user_id: 2, answer_date: '2019-11-19T20:15:36.399Z'},
-        {answer: 2, question_id: 11, user_id: 2, answer_date: '2019-11-19T20:15:36.399Z'},
 
-      ]);
+      const getRandomInt = (min, max) =>{
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+      }
+
+      function randomDate(start, end) {
+        return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+      }
+    
+    randomDate(new Date(2012, 0, 1), new Date())
+
+      const fakeAnswers = []
+      for(let i = 0 ; i < 10000;i++){
+        fakeAnswers.push({
+          answer:getRandomInt(1,10),
+          question_id:getRandomInt(1,13),
+          user_id:getRandomInt(1,827),
+          answer_date: randomDate(new Date(2019, 10, 1), new Date(2020, 10, 1))
+        })
+      }
+      // Inserts seed entries
+      return knex('answers').insert(fakeAnswers);
     });
 };
