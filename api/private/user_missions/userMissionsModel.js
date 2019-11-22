7245698de3f8ea0b5e73dc04a0b62e181b4f0e1b
roman-jordan <rjordan1352@gmail.com>
2019-11-21 23:32:46 -0800

    yo

--- api/private/user_missions/userMissionsModel.js
+++ api/private/user_missions/userMissionsModel.js
@@ -1,50 +1,52 @@
-const db = require(_dbConfig)
-module.exports={
-    findAll,
-    findById,
-    remove,
-    add,
-    editById
+const db = require(_dbConfig);
+module.exports = {
+  findAll,
+  findById,
+  remove,
+  add,
+  editById
+};
+const table = "user_missions";
+async function findAll(id) {
+
+ 
+  
+  const missionProgress = await
+      db('missions as m').select('mp.*')
+      .from( function (){
+          this.select(db.raw('array_agg(a.answer) as answers'),'m.vertical as mission')
+          .from('answers as a')
+          .join('missions as m','m.question','a.question_id')
+          .whereBetween("answer_date", ['2019-11-18', '2019-11-21'])
+          .andWhere('a.user_id',1)
+          .as('mp')
+          .groupBy('m.vertical')
+      })
+      
+     
+
+     
+
+
+  return {missionProgress: missionProgress}
 }
-const table='user_missions'
-async function findAll(id){
-    const userMissions = await db(table + ' as um')
-    .join('missions as m','m.id','um.mission_id')
-    .where('um.user_id',1)
-    
-    const missionProgress = []
-     await userMissions.forEach(async mission =>{
-         await db('answers as a')
-        .select('a.answer')
-        .where('a.user_id',2)
-        .andWhere('a.question_id',12)
-        .then(res =>{
-            console.log('sdafsdafsdfsdfsdfsfsfsdaffef',res) 
-            missionProgress.push(res)
-        })
-        .catch(err =>console.log(err))
-    })
-    
-    return {missions:[...userMissions],progress:[...missionProgress]}
-    
-}
-function findById(id){
-    return db(table)
-    .where({id})
-    .first()
+function findById(id) {
+  return db(table)
+    .where({ id })
+    .first();
 }
 function remove(id) {
-    return db(table)
-    .where({id})
-    .del()
-}
-function editById(id,update){
-    return db(table)
+  return db(table)
     .where({ id })
-    .update(update, '*');
+    .del();
 }
-function add(obj){
-    return db(table)
-    .insert(obj,'id')
-    .then(([id])=>findById(id))
+function editById(id, update) {
+  return db(table)
+    .where({ id })
+    .update(update, "*");
+}
+function add(obj) {
+  return db(table)
+    .insert(obj, "id")
+    .then(([id]) => findById(id));
 }
