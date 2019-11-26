7bcad7f49f9ae88cb25bc77fd48083f7a5037d82
roman-jordan <rjordan1352@gmail.com>
2019-11-26 10:16:35 -0800

    yo

--- api/private/answers/answersModel.js
+++ api/private/answers/answersModel.js
@@ -1,40 +1,38 @@
 const db = require(_dbConfig);
 module.exports = {
-    find,
-    findAll,
-    findByUserId,
-    add,
-    findByDateRange,
-    findAllByQuestionId
+  add,
+  find,
+  findAll,
+  findByDateRange,
+  findAllByQuestionId
 };
 
 const table = "answers";
 
 function find() {
-    return db(table);
+  return db(table);
 }
 
 function findAll(id) {
-    return db(table)
-        .where("user_id", id)
-        .first();
+  return db(table)
+    .where("user_id", id)
+    .first();
 }
 
 function findAllByQuestionId(id) {
-    return db(table)
-        .where({ id })
-        .where("user_id", req.user.userId)
-        .first();
+  return db(table)
+    .where({ id })
+    .where("user_id", req.user.userId)
+    .first();
 }
 
 function findByDateRange(id, startDate, endDate) {
-    return db(table)
-        .whereBetween("answer_date", [startDate, endDate])
-        .orderBy("user_id")
-        .where("user_id", id);
+  return db(table)
+    .whereBetween("answer_date", [startDate, endDate])
+    .orderBy("user_id")
+    .where("user_id", id);
 }
 
 function add(obj) {
-    return db(table)
-        .insert(obj, "id")
+  return db(table).insert(obj, "id");
 }
