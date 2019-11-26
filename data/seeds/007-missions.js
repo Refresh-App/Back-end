812641f366f72a9f23963e3b6b5674ace163c4f4
roman-jordan <rjordan1352@gmail.com>
2019-11-26 14:12:00 -0800

    yo

--- data/seeds/007-missions.js
+++ data/seeds/007-missions.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('missions').truncate()
+  return knex('missions').del()
     .then(function () {
       // Inserts seed entries
       return knex('missions').insert([
--- data/seeds/008-userMissions.js
+++ data/seeds/008-userMissions.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('user_missions').truncate()
+  return knex('user_missions').del()
     .then(function () {
       // Inserts seed entries
       return knex('user_missions').insert([
