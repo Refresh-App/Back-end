8c984da9babfbcd6e9134ddca8cfbcba896fb2f8
roman-jordan <rjordan1352@gmail.com>
2019-11-29 06:35:51 -0800

    Sync

--- data/seeds/006_questiongroup.js
+++ data/seeds/006_questiongroup.js
@@ -5,10 +5,12 @@ exports.seed = function(knex) {
     .then(function () {
 
       // Sqlite 3 does not support Int[]
-      const questions = process.env.DB_ENV === 'test' 
-      || process.env.DB_ENV === 'development'
-      ? '[1,2,3,4,5,6,7,8,9,10]'
-      : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      const questions = process.env.DB_ENV === 'staging' 
+      || process.env.DB_ENV === 'production'
+      ? [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      : '[1,2,3,4,5,6,7,8,9,10]'
+      
+      console.log(process.env.NODE_ENV)
 
       return knex('question_groups').insert([
         {group: 'onboarding', question_ids: questions}
