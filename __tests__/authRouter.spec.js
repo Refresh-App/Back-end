3d12f15040947c5a9d52c81c8505f275767ed00c
roman-jordan <rjordan1352@gmail.com>
2019-11-29 18:20:41 -0800

    Update

--- __tests__/authRouter.spec.js
+++ __tests__/authRouter.spec.js
@@ -4,11 +4,12 @@ const req = supertest(app);
 
 describe("It should Register and Log in a user", () => {
   it("Default Route", async done => {
+    
     const res = await req.post("/register").send({
         email:'testyMc@Testerson.com',
         password:"secretSquirl"
     });
-    console.log('post',res.body)
+    
     expect(res.status).toBe(201);
     
     done();
