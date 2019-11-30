7497ee3b965414af1c9a1d67417c953b2a4feca1
roman-jordan <rjordan1352@gmail.com>
2019-11-29 18:03:22 -0800

    Update

--- __tests__/authRouter.spec.js
+++ __tests__/authRouter.spec.js
@@ -2,15 +2,12 @@ const app = require("../index"); // Link to your server file
 const supertest = require("supertest");
 const req = supertest(app);
 
-describe("It should Register and Log in a user", () => {
+describe("Renders Without Crashing", () => {
   it("Default Route", async done => {
-    const res = await req.post("/register").send({
-        email:'testyMc@Testerson.com',
-        password:"secretSquirl"
-    });
+    const res = await req.get("/");
 
     expect(res.status).toBe(200);
-    
+
     done();
   });
 });
