1bd4ada3df5c3ba661088f2b9d02c590f04d8262
roman-jordan <rjordan1352@gmail.com>
2019-11-30 09:27:21 -0800

    Switched to stage

--- knexfile.js
+++ knexfile.js
@@ -1,24 +1,14 @@
 module.exports = {
-  test: {
-    client: "sqlite3",
-    useNullAsDefault: true,
-    connection: {
-      filename: "./data/dev.sqlite3"
-    },
-    migrations: {
-      directory: "./data/migrations"
-    },
-    seeds: {
-      directory: "./data/seeds"
-    }
-  },
+  
   development: {
-    client: "sqlite3",
-    useNullAsDefault: true,
-    connection: {
-      filename: "./data/dev.sqlite3"
+    client: "pg",
+    connection: process.env.DATABASE_URL_STAGE,
+    pool: {
+      min: 2,
+      max: 10
     },
     migrations: {
+      tableName: "knex_migrations",
       directory: "./data/migrations"
     },
     seeds: {
