0a3c8ac38803be3b853111d5b0083261fa9b8680
roman-jordan <rjordan1352@gmail.com>
2019-12-11 19:16:38 -0800

    Fixed

--- api/public/docs/docProcessor.js
+++ api/public/docs/docProcessor.js
@@ -6,41 +6,6 @@ module.exports = {
 };
 
 function docGen(routes) {
-<<<<<<< HEAD
-    return (req, res, next) => {
-        let newObj = {};
-        Object.keys(routes).forEach(async routeGroup => {
-            axiosCalls = [];
-            routes[routeGroup].forEach((route, i) => {
-                axiosCalls.push(
-                    axios[route.method.toLowerCase()](rootUrl + route.route)
-                );
-            });
-            const resolved = [];
-            await Promise.all(
-                axiosCalls.map((p, i) =>
-                    p
-                    .then(res =>
-                        resolved.push({...routes[routeGroup][i], returns: res.data })
-                    )
-                    .catch(() =>
-                        resolved.push({
-                            ...routes[routeGroup][i],
-                            returns: "Not Functioning"
-                        })
-                    )
-                )
-            );
-            newObj[routeGroup] = resolved;
-            console.log(newObj);
-        });
-        //New object Returned before Above is fully resolved
-        req.routes = newObj;
-        setTimeout(function() { next(); }, 3000);
-
-    };
-}
-=======
   const token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlblR5cGUiOiJCYXNpYyAiLCJ1c2VyX2lkIjoxMSwidXNlcl9yb2xlcyI6W3siaWQiOjJ9XSwiaWF0IjoxNTc1OTMyOTE2LCJleHAiOjE1NzY1Mzc3MTZ9.toMHY7uf9V00xcgWRaYXOZ6OVPJNX3-tJQd2Pn-6HnI";
   return (req, res, next) => {
@@ -114,4 +79,3 @@ function docGen(routes) {
     }, 3000);
   };
 }
->>>>>>> 84ef1d5946c89296f0c16a3512357242d40cb79a
