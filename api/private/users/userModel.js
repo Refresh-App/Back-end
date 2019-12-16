cbb2e14b2e80ad3fa5ff93755aad341066b0e5c5
roman-jordan <rjordan1352@gmail.com>
2019-12-16 13:38:09 -0800

    yo

--- api/private/users/userModel.js
+++ api/private/users/userModel.js
@@ -11,7 +11,9 @@ module.exports = {
 const table = "users";
 
 function findAll() {
-  return db(table);
+  return db(table + " as u")
+  .select('*')
+  .join("profile as p","p.user_id","u.id")
 }
 function findById(id) {
  
