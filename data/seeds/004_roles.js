7cb99364e2f848f7a30aaddf664097a5978b8e8b
Jeremy Brady <jbdigitalgeek@gmail.com>
2019-12-16 13:36:02 -0800

    yo

--- data/seeds/004_roles.js
+++ data/seeds/004_roles.js
@@ -1,15 +1,14 @@
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex("roles")
-    .del()
-    .then(function() {
-      // Inserts seed entries
-      return knex("roles").insert([
-        { role: "admin" },
-        { role: "user" },
-        { role: "section_lead" },
-        { role: "section_lead" }
-      ]);
-    });
+    // Deletes ALL existing entries
+    return knex("roles")
+        .del()
+        .then(function() {
+            // Inserts seed entries
+            return knex("roles").insert([
+                { role: "admin" },
+                { role: "user" },
+                { role: "section_lead" },
+                { role: "team_lead" }
+            ]);
+        });
 };
-
