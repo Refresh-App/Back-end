819ae578c23e737ed91d9adf3ad888c573247b78
Jeremy Brady <jbdigitalgeek@gmail.com>
2019-12-20 10:48:27 -0800

    removed unique from name

--- data/migrations/20191113182250_questiongroups.js
+++ data/migrations/20191113182250_questiongroups.js
@@ -1,7 +1,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable("question_groups", col => {
     col.increments();
-    col.text("name", 25).unique();
+    col.text("name", 25);
     col.text("description", 50)
     col.specificType("question_ids", "INT[]").notNullable();
     col.integer('user_id')
