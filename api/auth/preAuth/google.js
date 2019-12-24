1460210b9e1212b501b5a5f51097713c51abf1d2
roman-jordan <rjordan1352@gmail.com>
2019-12-23 18:24:29 -0800

    testing

--- api/auth/preAuth/google.js
+++ api/auth/preAuth/google.js
@@ -6,7 +6,7 @@ const jwt = require("./jwt");
 //Config GitHub Auth
 const googleId = process.env.GOOGLE_CLIENT_ID;
 const googleSecret = process.env.GOOGLE_CLIENT_SECRET;
-const googleRedirect = "https://apidevnow.com/googleAuth/return";
+const googleRedirect = `https://${_URL}/facebookAuth/return`;
 
 //Bring in the userModel and profileScrubber
 const User = require("../authModel");
