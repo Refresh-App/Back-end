02f4fbf0c858a485096255f8b5626f7df1cfd6ee
roman-jordan <rjordan1352@gmail.com>
2020-01-11 13:28:18 -0800

    Checking put scrubber

--- api/private/profile/profile.js
+++ api/private/profile/profile.js
@@ -23,7 +23,7 @@ router.put("/", (req, res) => {
             res.status(200).json({ message: `Success`, ...profile });
         })
         .catch(e => {
-            res.status(200).json({ message: "Something has gone wrong", ...e });
+            res.status(200).json({ message: "Something has gone wrong", ...e,request:{...req.body} });
         });
 });
 
