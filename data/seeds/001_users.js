93e2b0a70eb9875657c492927203b2c8a957b4df
Jeremy Brady <jbdigitalgeek@gmail.com>
2020-01-12 12:37:01 -0800

    changing seeds

--- data/seeds/001_users.js
+++ data/seeds/001_users.js
@@ -1,25 +1,62 @@
 require("dotenv").config();
 const bcrypt = require("bcrypt");
 const HashFactor = 8;
-const URL = process.env.ROOT_URL || "apidevnow.com";
+const URL = "apidevnow.com";
 
-const createFakeUser = count => ({
-  email: `testuser${count}@${URL}`,
-  password: bcrypt.hashSync("roman", HashFactor)
+const seedTeam = [
+  {
+    display_name: "Bob",
+    avatar:
+      "https://res.cloudinary.com/thinkmode/image/upload/v1578802931/dog_wyubak.jpg",
+    bio: "Where are the cookies?",
+    user_id: 2
+  },
+  {
+    display_name: "Jamie",
+    avatar:
+      "https://res.cloudinary.com/thinkmode/image/upload/v1578802931/party_w45wo9.jpg",
+    bio: "Where's my car dude?",
+    user_id: 3
+  },
+  {
+    display_name: "Charlotte",
+    avatar:
+      "https://res.cloudinary.com/thinkmode/image/upload/v1578802931/skull_fyf6v7.jpg",
+    bio: "Code or die",
+    user_id: 4
+  },
+  {
+    display_name: "Joe Bob Dandy",
+    avatar:
+      "https://res.cloudinary.com/thinkmode/image/upload/v1578802930/jeebs_zmtsqs.jpg",
+    bio: "What day is it?",
+    user_id: 5
+  },
+  {
+    display_name: "Lucy",
+    avatar:
+      "https://res.cloudinary.com/thinkmode/image/upload/v1578803665/naughty_vt26fs.jpg",
+    bio: "-_-",
+    user_id: 6
+  }
+];
+const createFakeUser = seedTeam.map(function(member) {
+  return {
+    display_name: member.display_name,
+    avatar: member.avatar,
+    bio: member.bio,
+    user_id: member.user_id
+  };
 });
-
+console.log(createFakeUser)
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex("users")
     .del()
     .then(function() {
-      const fakeUsers = [];
-      const userCount = 6;
+     
 
-      for (let i = 0; i < userCount; i++) {
-        fakeUsers.push(createFakeUser(i));
-      }
       // Inserts seed entries
-      return knex("users").insert(fakeUsers);
+      return knex("users").insert(createFakeUser);
     });
 };
